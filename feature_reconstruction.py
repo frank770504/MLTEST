f7e621775cddb438a3d1b9b562004b5cf47c38de
Frank.YW Chen(陳衍文) <frank.yw_chen@htc.com>
2016-06-16 20:56:19 +0800

    change string to int
    if NULL rewritten as -1

--- feature_reconstruction.py
+++ feature_reconstruction.py
@@ -22,19 +22,37 @@ def Read2Area():
       area_feature = np.vstack([area_feature, row_np]) if area_feature.size>0 else row_np
   return area_feature
 
+def DataStr2Int(mat):
+  mat_int = mat.astype(int)
+  _sz_r, _sz_c = mat_int.shape
+  for i in range(0, _sz_r):
+    if mat[i,0] == 'NULL':
+      mat_int[i,:] = np.ones(_sz_c) * -1
+  return mat_int
+    
 # test desc here
 g_area_feature = Read2Area()
 g_date = g_area_feature[:,0]
-g_order = g_area_feature[:,1:3]
-g_traff = g_area_feature[:,4:7]
-g_other = g_area_feature[:,8:10]
-g_y = g_area_feature[:,11:12]
+g_order = g_area_feature[:,1:4]
+g_traff = g_area_feature[:,4:8]
+g_other = g_area_feature[:,8:11]
+g_y = g_area_feature[:,11:13]
 
-for i in range(300,302):
+#~ g_date_int = DataStr2Int(g_date)
+g_order_int = DataStr2Int(g_order)
+g_traff_int = DataStr2Int(g_traff)
+g_other_int = DataStr2Int(g_other)
+g_y_int = DataStr2Int(g_y)
+# string to numbers
+# detect NULL
+# interpolation
+
+for i in range(96,98):
+  print "-----{}-----".format(i)
   print g_date[i]
-  print g_order[i]
-  print g_traff[i]
-  print g_other[i]
-  print g_y[i]
+  print g_order_int[i]
+  print g_traff_int[i]
+  print g_other_int[i]
+  print g_y_int[i]
 
 # main here
